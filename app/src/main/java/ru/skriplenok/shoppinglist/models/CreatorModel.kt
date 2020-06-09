1ffee28c20f1330d80e774fe26e9a63d57f30795
AlexeySk88 <skriplenok88@mail.ru>
2020-06-10 00:23:24 +0300

    CreatorModel добавлены Bindadle поля

--- app/src/main/java/ru/skriplenok/shoppinglist/models/CreatorModel.kt
+++ app/src/main/java/ru/skriplenok/shoppinglist/models/CreatorModel.kt
@@ -1,10 +1,28 @@
 package ru.skriplenok.shoppinglist.models
 
-data class CreatorModel(
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import ru.skriplenok.shoppinglist.BR
+
+class CreatorModel(
     var title: String?,
     var productList: MutableList<ProductModel>,
-    var productName: String?,
-    var productQuantity: String?,
+    _productName: String?,
+    _productQuantity: String?,
     var indexType: Int
-) {
+): BaseObservable() {
+
+    @Bindable
+    var productName: String? = _productName
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.productName)
+    }
+
+    @Bindable
+    var productQuantity: String? = _productQuantity
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.productQuantity)
+    }
 }
